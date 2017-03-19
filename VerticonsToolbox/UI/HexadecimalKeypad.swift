24b6fb1903bb1ab76beec2688e9f133979138c7c
Robert Vaessen <robert@rvaessen.com>
2017-03-19 13:51:23 -0400

    The Bundle and UIView nib loading extensions were problamatic. Improved Hexidecimal input.

---
+++ VerticonsToolbox/UI/HexadecimalKeypad.swift
@@ -0,0 +1,83 @@
+//
+//  HexadecimalKeypad
+//  VerticonsToolbox
+//
+//  Created by Robert Vaessen on 3/10/17.
+//  Copyright © 2017 Verticon. All rights reserved.
+//
+
+import UIKit
+
+public enum HexadecimalKey : Int {
+    case zero
+    case one
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case A
+    case B
+    case C
+    case D
+    case E
+    case F
+    case del
+    case done
+    
+    public func toString() -> String {
+        switch self {
+        case .del:
+            return "del"
+        case .done:
+            return "done"
+        default:
+            return String(format:"%X", rawValue)
+        }
+    }
+}
+
+public protocol HexadecimalKeypadDelegate : class {
+    func newKey(_: HexadecimalKey)
+}
+
+public class HexadecimalKeypad: UIView {
+    
+
+    @IBOutlet private var view: UIView!
+
+    public weak var delegate: HexadecimalKeypadDelegate?
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addNibView()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        addNibView()
+    }
+
+    @IBAction func keyTapped(_ sender: UIButton) {
+        self.delegate?.newKey(HexadecimalKey(rawValue: sender.tag)!)
+    }
+
+    private func addNibView() {
+        let bundle = Bundle(for: type(of: self))
+        UINib(nibName: String(describing: type(of: self)), bundle: bundle).instantiate(withOwner: self, options: nil)
+        
+        addSubview(view) // Add the nib's view as a sub-view
+        view.frame = bounds // Have the sub-view fill this view
+    }
+
+    public static func setup(forTextField: UITextField, withDelegate: HexadecimalKeypadDelegate) {
+        let inputView = HexadecimalKeypad(frame: CGRect(x: 0, y: 0, width: 0, height: 300))
+        forTextField.inputView = inputView
+        inputView.delegate = withDelegate
+    }
+}
--- VerticonsToolbox/UI/View.swift
+++ VerticonsToolbox/UI/View.swift
@@ -59,6 +59,7 @@ public extension UIView {
     }
 }
 
+/*
 public extension UIView {
     /*
      let myCustomView = CustomView.fromNib() // NIB named "CustomView"
@@ -71,7 +72,50 @@ public extension UIView {
      2) For the outermost View - In the Identity Inspector, set the Custom Class to the desired class
      3) For the outermost View - In the Connection Inspector, make the connections to the subviews in the Document Outline.
      */
-    
+
+    /* Throws an exception if the view specifies connections because no owner is provided to which to wire the outlets
+        Strangely, it seems to be okay for the nib to specify actions.
+
+
+     2017-03-18 17:04:24.122 Nibs[51960:29042640] *** Terminating app due to uncaught exception 'NSUnknownKeyException', reason: '[<NSObject 0x61000000b7c0> setValue:forUndefinedKey:]: this class is not key value coding-compliant for the key clickMe.'
+     *** First throw call stack:
+     (
+     0   CoreFoundation                      0x000000010be96d4b __exceptionPreprocess + 171
+     1   libobjc.A.dylib                     0x00000001098d921e objc_exception_throw + 48
+     2   CoreFoundation                      0x000000010be96c99 -[NSException raise] + 9
+     3   Foundation                          0x00000001093e79df -[NSObject(NSKeyValueCoding) setValue:forKey:] + 291
+     4   UIKit                               0x000000010a1b779e -[UIRuntimeOutletConnection connect] + 109
+     5   CoreFoundation                      0x000000010be3b9e0 -[NSArray makeObjectsPerformSelector:] + 256
+     6   UIKit                               0x000000010a1b6122 -[UINib instantiateWithOwner:options:] + 1867
+     7   UIKit                               0x000000010a1b83bb -[NSBundle(UINSBundleAdditions) loadNibNamed:owner:options:] + 223
+     8   VerticonsToolbox                    0x00000001093482f6 _TZFE16VerticonsToolboxCSo6UIView7fromNibuRxS0_rfTGSqSS_4typeMx_GSqx_ + 1158
+     9   VerticonsToolbox                    0x0000000109347c83 _TZFE16VerticonsToolboxCSo6UIView7fromNibuRxS0_rfTGSqSS_4typeMx_x + 163
+     10  VerticonsToolbox                    0x0000000109347bb1 _TZFE16VerticonsToolboxCSo6UIView7fromNibfGSqSS_DS0_ + 145
+     11  VerticonsToolbox                    0x0000000109347e0c _TToZFE16VerticonsToolboxCSo6UIView7fromNibfGSqSS_DS0_ + 204
+     12  Nibs                                0x0000000109299b47 _TFC4Nibs18NibsViewController11viewDidLoadfT_T_ + 711
+     13  Nibs                                0x0000000109299d02 _TToFC4Nibs18NibsViewController11viewDidLoadfT_T_ + 34
+     14  UIKit                               0x0000000109f4aa3d -[UIViewController loadViewIfRequired] + 1258
+     15  UIKit                               0x0000000109f4ae70 -[UIViewController view] + 27
+     16  UIKit                               0x0000000109e144b5 -[UIWindow addRootViewControllerViewIfPossible] + 71
+     17  UIKit                               0x0000000109e14c06 -[UIWindow _setHidden:forced:] + 293
+     18  UIKit                               0x0000000109e28519 -[UIWindow makeKeyAndVisible] + 42
+     19  UIKit                               0x0000000109da0f8d -[UIApplication _callInitializationDelegatesForMainScene:transitionContext:] + 4818
+     20  UIKit                               0x0000000109da70ed -[UIApplication _runWithMainScene:transitionContext:completion:] + 1731
+     21  UIKit                               0x0000000109da426d -[UIApplication workspaceDidEndTransaction:] + 188
+     22  FrontBoardServices                  0x000000010f3516cb __FBSSERIALQUEUE_IS_CALLING_OUT_TO_A_BLOCK__ + 24
+     23  FrontBoardServices                  0x000000010f351544 -[FBSSerialQueue _performNext] + 189
+     24  FrontBoardServices                  0x000000010f3518cd -[FBSSerialQueue _performNextFromRunLoopSource] + 45
+     25  CoreFoundation                      0x000000010be3b761 __CFRUNLOOP_IS_CALLING_OUT_TO_A_SOURCE0_PERFORM_FUNCTION__ + 17
+     26  CoreFoundation                      0x000000010be2098c __CFRunLoopDoSources0 + 556
+     27  CoreFoundation                      0x000000010be1fe76 __CFRunLoopRun + 918
+     28  CoreFoundation                      0x000000010be1f884 CFRunLoopRunSpecific + 420
+     29  UIKit                               0x0000000109da2aea -[UIApplication _run] + 434
+     30  UIKit                               0x0000000109da8c68 UIApplicationMain + 159
+     31  Nibs                                0x000000010929b66f main + 111
+     32  libdyld.dylib                       0x000000010dcd468d start + 1
+     )
+     libc++abi.dylib: terminating with uncaught exception of type NSException
+     */
     public class func fromNib(_ nibNameOrNil: String? = nil) -> Self {
         return fromNib(nibNameOrNil, type: self)
     }
@@ -82,27 +126,7 @@ public extension UIView {
     }
     
     public class func fromNib<T : UIView>(_ nibNameOrNil: String? = nil, type: T.Type) -> T? {
-/*
-        var view: T?
 
-         let name: String
-        if let nibName = nibNameOrNil {
-            name = nibName
-        } else {
-            // Most nibs are demangled by practice, if not, just declare string explicitly
-            name = nibName
-        }
-
-         let bundle = Bundle(for: T.self)
-         let nibViews = bundle.loadNibNamed(name, owner: nil, options: nil)
-         for v in nibViews! {
-         if let tog = v as? T {
-         view = tog
-         }
-         }
-         
-         return view
-*/
         let name = nibNameOrNil == nil ? nibName : nibNameOrNil!
         
         if let views = Bundle(for: type).loadNibNamed(name, owner: nil) {
@@ -127,3 +151,4 @@ public extension UIView {
         }
     }
 }
+ */
