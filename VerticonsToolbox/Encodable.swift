7d6f34e9d694303f1b871059fc359cf26b6b1538
Robert Vaessen <robert@rvaessen.com>
2017-04-04 14:15:19 -0400

    Improved Encodable protocol

--- VerticonsToolbox/Encodable.swift
+++ VerticonsToolbox/Encodable.swift
@@ -8,21 +8,72 @@
 
 import Foundation
 
+/*
+ Types announce their encodability by adopting the Encodable protocol. The creation of the
+ protocol is motivated by the fact that swift's struct types cannot adopt the NSCoding protocol
+ and thus NSKeyedArchiver is not available.
+ */
+
 public protocol Encodable {
     typealias Properties = Dictionary<String, Any>
     func encode() -> Properties
     init?(_ properties: Properties?)
 }
 
-public func saveToUserDefaults<T:Encodable>(_ values: [T], withKey key: String) {
-    let encodings = values.map{ $0.encode() }
-    UserDefaults.standard.set(encodings, forKey: key)
-}
 
-public func loadFromUserDefaults<T:Encodable>(type: T.Type, withKey key: String) -> [T] {
-    guard let encodings = UserDefaults.standard.array(forKey: key) else { return [] }
-    return encodings.flatMap {
-        guard let properties = $0 as? Encodable.Properties else { return nil }
-        return T(properties)
+// It will often occur that we have a array of objects to be encoded.
+// These extensions will make the usage at the call site a bit cleaner.
+
+extension Array where Element == Encodable.Properties {
+    public func decode<T:Encodable>(type: T.Type) -> [T] {
+        return flatMap{ T($0) }
     }
 }
+
+extension Array where Element : Encodable {
+    public func encode() -> [Encodable.Properties] {
+        return map{ $0.encode() }
+    }
+}
+
+
+// These top level functions will save/load arrays of Encodable objecst to/from a file in the document directory.
+
+public func saveToUserDefaults<T:Encodable>(_ objects: [T], withKey key: String) {
+    UserDefaults.standard.set(objects.encode(), forKey: key)
+}
+
+public func loadFromUserDefaults<T:Encodable>(type: T.Type, withKey key: String) -> [T]? {
+    return (UserDefaults.standard.array(forKey: key) as? [Encodable.Properties])?.decode(type: T.self)
+}
+
+
+// These top level functions will save/load arrays of Encodable objecst to/from a file in the document directory.
+
+public func saveToFile<T:Encodable>(_ objects: [T], withName name: String) -> Bool {
+    do {
+        let data = NSKeyedArchiver.archivedData(withRootObject: objects.encode())
+        try data.write(to: getUrl(forName: name), options: .atomic)
+        return true
+    } catch {
+        print(error)
+        return false
+    }
+}
+
+public func loadFromFile<T:Encodable>(type: T.Type, withName name: String) -> [T]? {
+    do {
+        let data = try Data(contentsOf: getUrl(forName: name))
+        if let encoded = NSKeyedUnarchiver.unarchiveObject(with: data) as? [Encodable.Properties] {
+            return encoded.decode(type: type)
+        }
+    } catch {
+        print(error)
+    }
+    return nil
+}
+
+private func getUrl(forName: String) -> URL {
+    return try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(forName)
+}
+
