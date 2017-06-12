ff327943f201bb3730984240789658e3e2d2dbc1
Robert Vaessen <robert@rvaessen.com>
2017-06-12 09:45:25 -0400

    Eliminated the need for a separate wrapper object from string encoding.

--- VerticonsToolbox/String.swift
+++ VerticonsToolbox/String.swift
@@ -8,62 +8,57 @@
 
 import Foundation
 
-public extension String.Encoding {
-
-    public struct Wrapper : CustomStringConvertible{
-        public let encoding: String.Encoding
-        public var name: String
-        public var description: String { return name }
-    }
-
-    /// All possible values of String.Encoding
-    public static let all: [Wrapper] = {
-        var wrappers = [Wrapper]()
+public struct StringEncoding : RawRepresentable, CustomStringConvertible {
+    
+    private static var encodings: [String.Encoding]  = [.ascii, .iso2022JP, .isoLatin1, .isoLatin2, .japaneseEUC, .macOSRoman,
+                                                        .nextstep, .nonLossyASCII, .shiftJIS, .symbol, .unicode, .utf16, .utf16BigEndian,
+                                                        .utf16LittleEndian, .utf32BigEndian, .utf32LittleEndian, .utf8, .windowsCP1250,
+                                                        .windowsCP1251, .windowsCP1252, .windowsCP1253, .windowsCP1254]
+    public static var all: [StringEncoding]  = {
+        return encodings.map { StringEncoding(rawValue: $0)! }
         
-        wrappers.append(Wrapper(encoding: String.Encoding.ascii, name: "ascii"))
-        wrappers.append(Wrapper(encoding: String.Encoding.iso2022JP, name: "iso2022JP"))
-        wrappers.append(Wrapper(encoding: String.Encoding.isoLatin1, name: "isoLatin1"))
-        wrappers.append(Wrapper(encoding: String.Encoding.isoLatin2, name: "isoLatin2"))
-        wrappers.append(Wrapper(encoding: String.Encoding.japaneseEUC, name: "japaneseEUC"))
-        wrappers.append(Wrapper(encoding: String.Encoding.macOSRoman, name: "macOSRoman"))
-        wrappers.append(Wrapper(encoding: String.Encoding.nextstep, name: "nextstep"))
-        wrappers.append(Wrapper(encoding: String.Encoding.nonLossyASCII, name: "nonLossyASCII"))
-        wrappers.append(Wrapper(encoding: String.Encoding.shiftJIS, name: "shiftJIS"))
-        wrappers.append(Wrapper(encoding: String.Encoding.symbol, name: "symbol"))
-        wrappers.append(Wrapper(encoding: String.Encoding.unicode, name: "unicode"))
-        wrappers.append(Wrapper(encoding: String.Encoding.utf16, name: "utf16"))
-        wrappers.append(Wrapper(encoding: String.Encoding.utf16BigEndian, name: "utf16BigEndian"))
-        wrappers.append(Wrapper(encoding: String.Encoding.utf16LittleEndian, name: "utf16LittleEndian"))
-        wrappers.append(Wrapper(encoding: String.Encoding.utf32, name: "utf32"))
-        wrappers.append(Wrapper(encoding: String.Encoding.utf32BigEndian, name: "utf32BigEndian"))
-        wrappers.append(Wrapper(encoding: String.Encoding.utf32LittleEndian, name: "utf32LittleEndian"))
-        wrappers.append(Wrapper(encoding: String.Encoding.utf8, name: "utf8"))
-        wrappers.append(Wrapper(encoding: String.Encoding.windowsCP1250, name: "windowsCP1250"))
-        wrappers.append(Wrapper(encoding: String.Encoding.windowsCP1251, name: "windowsCP1251"))
-        wrappers.append(Wrapper(encoding: String.Encoding.windowsCP1252, name: "windowsCP1252"))
-        wrappers.append(Wrapper(encoding: String.Encoding.windowsCP1253, name: "windowsCP1253"))
-        wrappers.append(Wrapper(encoding: String.Encoding.windowsCP1254, name: "windowsCP1254"))
-        
-        return wrappers
     }()
-
-    /// A textual name for this encoding
-    public var name: String? {
-        for wrapper in String.Encoding.all {
-            if self == wrapper.encoding {
-                return wrapper.name
-            }
+    
+    public let rawValue: String.Encoding
+    
+    public init?(rawValue: String.Encoding) {
+        guard StringEncoding.encodings.contains(rawValue) else { return nil }
+        self.rawValue = rawValue
+        switch rawValue {
+        case String.Encoding.ascii: name = "ascii"
+        case String.Encoding.iso2022JP: name = "iso2022JP"
+        case String.Encoding.isoLatin1: name = "isoLatin1"
+        case String.Encoding.isoLatin2: name = "isoLatin2"
+        case String.Encoding.japaneseEUC: name = "japaneseEUC"
+        case String.Encoding.macOSRoman: name = "macOSRoman"
+        case String.Encoding.nextstep: name = "nextstep"
+        case String.Encoding.nonLossyASCII: name = "nonLossyASCII"
+        case String.Encoding.shiftJIS: name = "shiftJIS"
+        case String.Encoding.symbol: name = "symbol"
+        case String.Encoding.unicode: name = "unicode"
+        case String.Encoding.utf16: name = "utf16"
+        case String.Encoding.utf16BigEndian: name = "utf16BigEndian"
+        case String.Encoding.utf16LittleEndian: name = "utf16LittleEndian"
+        case String.Encoding.utf32: name = "utf32"
+        case String.Encoding.utf32BigEndian: name = "utf32BigEndian"
+        case String.Encoding.utf32LittleEndian: name = "utf32LittleEndian"
+        case String.Encoding.utf8: name = "utf8"
+        case String.Encoding.windowsCP1250: name = "windowsCP1250"
+        case String.Encoding.windowsCP1251: name = "windowsCP1251"
+        case String.Encoding.windowsCP1252: name = "windowsCP1252"
+        case String.Encoding.windowsCP1253: name = "windowsCP1253"
+        case String.Encoding.windowsCP1254: name = "windowsCP1254"
+        default: name = "<Unrecognized String.Encoding>"
         }
-        return nil
     }
 
-    /// The index of this encoding in the String.Encoding.all array
+    public let  name: String
+
+    
+    public var description: String { return self.name }
+    
+    /// The index of this StringEncoding in the StringEncoding.all array
     public var index: Int? {
-        for index in 0 ..< String.Encoding.all.count {
-            if self == String.Encoding.all[index].encoding {
-                return index
-            }
-        }
-        return nil
+        return StringEncoding.all.index { self == $0 }
     }
 }
